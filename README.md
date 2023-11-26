ebd53ad34c31aa55e9edd3d946861dbfb8cea9f3
Md Abdur Razzaq Masum <masummdar@gmail.com>
2023-11-26 20:44:14 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Khata
+Khata is a note taking plugin for WordPress. With this you can create notebooks and notes inside those notebooks. All of this with native WordPress inferface.
---
+++ khata.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @package Khata
+ */
+/**
+ * Plugin Name: Khata
+ * Plugin URI: https://masummdar.com/projects/khata/
+ * Description: Write notes and organize them into notebooks right from WordPress dashboard.
+ * Version: 0.1.0
+ * Author: Md Abdur Razzaq Masum
+ * Author URI: https://masummdar.com/
+ * License: GPLv2 or later
+ * Text Domain: khata
+ */
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2023 Md Abdur Razzaq Masum.
+*/
